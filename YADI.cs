b491f6df2d3af6eed72a9594a1f92b825c35c2a0
Dillan Hildebrand <dillan.hildebrand@protonmail.com>
2021-12-28 21:37:12 -0700

    --allow-empty

--- YADI.cs
+++ YADI.cs
@@ -1,21 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
-using System.Timers;
-using System.Text.RegularExpressions;
 
 using YADI.Injection;
 using YADI.Enums;
-using System.IO;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+
 
 namespace YADI
 {
