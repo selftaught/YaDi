2081763589c51cb27dd3c7b8211a4d70ff385c4e
Dillan Hildebrand <dillan.hildebrand@protonmail.com>
2022-01-01 21:11:10 -0700

    removed Epoch() from Helpers.Misc

--- Helpers/Misc.cs
+++ Helpers/Misc.cs
@@ -4,10 +4,6 @@ namespace YADI.Helpers
 {
     class Misc
     {
-        public static double Epoch()
-        {
-            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
-            return t.TotalSeconds;
-        }
+
     }
 }
