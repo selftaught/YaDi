8dcc47f5bcf2b0345335a46aac378ac9a2be8f4b
Dillan Hildebrand <dillan.hildebrand@protonmail.com>
2022-01-04 09:57:40 -0700

    Added PeNet nuget package for handling PEs. Removed PortableExecParser helper in favor of PeNet.

--- Injectors/IATHook.cs
+++ Injectors/IATHook.cs
@@ -15,7 +15,15 @@ namespace YADI.Injection
 
         public override bool Inject(String sDllPath)
         {
-
+            /**
+             * 1. Get IAT
+             * 2. Find a function in the IAT to hook
+             * 3. Save the memory address of that function.
+             * 4. Create a hook/intercept function to replace the
+             *    function found in step #2.
+             * 5. Replace function found in step #2
+             *    with the hook/intercept function address.
+             */
             return true;
         }
     }
